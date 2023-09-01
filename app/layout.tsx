719d91c549aab1a5182dbceb136ce04cd4672a89
Inharul <88318546+inharul@users.noreply.github.com>
2023-09-01 14:46:01 +0530

    license

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,8 @@ const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
 export const metadata: Metadata = {
   title: "SolvexR",
-  description: "learning nextjs",
+  description:
+    "Solvexr is a webapp that lets you practice arithmetic operation based calculations. Each session lets you solve as many sums as you can under a time pressure, at the end of it, Accuracy, Average Time and Submissions are shown to correct mistakes.",
 };
 
 export default function RootLayout({
